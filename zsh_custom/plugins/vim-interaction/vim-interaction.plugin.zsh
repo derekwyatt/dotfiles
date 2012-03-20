51125ef97a3c21cadb9b86f0fa51a4509557f5f6
Derek Wyatt <derek@derekwyatt.org>
2012-03-19 20:37:26 -0400

    Exit 0??? /exit/ 0? Time to go to sleep...

--- zsh_custom/plugins/vim-interaction/vim-interaction.plugin.zsh
+++ zsh_custom/plugins/vim-interaction/vim-interaction.plugin.zsh
@@ -24,7 +24,7 @@ usage: callvim [-b cmd] [-a cmd] [file ... fileN]
   file       The file to edit
   ... fileN  The other files to add to the argslist
 EOH
-    exit 0
+    return 0
   fi
 
   local cmd=""
