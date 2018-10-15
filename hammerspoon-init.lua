17e6cdca12fe82bcec40125a5f5f606b777fb605
Derek Wyatt <derek@derekwyatt.org>
2018-10-15 09:31:43 -0400

    work layouts

--- hammerspoon-init.lua
+++ hammerspoon-init.lua
@@ -24,24 +24,42 @@ units = {
   left30        = { x = 0.0, y = 0.0, w = 0.3, h = 1.0 },
   top50         = { x = 0.0, y = 0.0, w = 1.0, h = 0.5 },
   bot50         = { x = 0.0, y = 0.5, w = 1.0, h = 0.5 },
+  bot80         = { x = 0.0, y = 0.2, w = 1.0, h = 0.8 },
   upright30     = { x = 0.7, y = 0.0, w = 0.3, h = 0.5 },
   botright30    = { x = 0.7, y = 0.5, w = 0.3, h = 0.5 },
   upleft70      = { x = 0.0, y = 0.0, w = 0.7, h = 0.5 },
   botleft70     = { x = 0.0, y = 0.5, w = 0.7, h = 0.5 },
   right70top80  = { x = 0.7, y = 0.0, w = 0.3, h = 0.8 },
-  maximum       = { x = 0.0, y = 0.0, w = 1.0, h = 1.0 }
+  maximum       = { x = 0.0, y = 0.0, w = 1.0, h = 1.0 },
+  center        = { x = 0.2, y = 0.1, w = 0.6, h = 0.8 }
 }
 
 layouts = {
+  alternatecoding = {
+    { name = 'Firefox', unit = units.left70 },
+    { name = 'VimR',    unit = units.left30 },
+    { name = 'iTerm2',  unit = units.right70 }
+  },
+  work = {
+    { name = 'Firefox',           unit = units.left70,  screen = 'Thunderbolt Display' },
+    { name = 'VimR',              unit = units.left30,  screen = 'Thunderbolt Display' },
+    { name = 'iTerm2',            unit = units.right70, screen = 'Thunderbolt Display' },
+    { name = 'Slack',             unit = units.bot80,   screen = 'Color LCD' },
+    { name = 'Microsoft Outlook', unit = units.maximum, screen = 'Color LCD' },
+    { name = 'WhatsApp',          unit = units.center,  screen = 'Color LCD' }
+  },
   coding = {
     { name = 'Firefox', unit = units.left70 },
     { name = 'VimR',    unit = units.left70 },
     { name = 'iTerm2',  unit = units.right30 }
   },
-  alternatecoding = {
-    { name = 'Firefox', unit = units.left70 },
-    { name = 'VimR',    unit = units.left30 },
-    { name = 'iTerm2',  unit = units.right70 }
+  work = {
+    { name = 'Firefox',           unit = units.left70,  screen = 'Thunderbolt Display' },
+    { name = 'VimR',              unit = units.left70,  screen = 'Thunderbolt Display' },
+    { name = 'iTerm2',            unit = units.right30, screen = 'Thunderbolt Display' },
+    { name = 'Slack',             unit = units.bot80,   screen = 'Color LCD' },
+    { name = 'Microsoft Outlook', unit = units.maximum, screen = 'Color LCD' },
+    { name = 'WhatsApp',          unit = units.center,  screen = 'Color LCD' }
   },
   writing = {
     { name = 'Firefox', unit = units.left70 },
@@ -51,11 +69,26 @@ layouts = {
   }
 }
 
+function isWorkMachine()
+  local allNames = hs.host.names()
+  for i=1,#allNames do
+    if allNames[i]:match('^cawl') then
+      return true
+    end
+  end
+  return false
+end
+
+
 function runLayout(layout)
   for i = 1,#layout do
     local t = layout[i]
     local win = hs.application.get(t.name):mainWindow()
-    win:move(t.unit, nil, true)
+    local screen = nil
+    if t.screen ~= nil then
+      screen = hs.screen.find(t.screen)
+    end
+    win:move(t.unit, screen, true)
   end
 end
 
@@ -70,8 +103,13 @@ hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.botlef
 hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
 
-hs.hotkey.bind(mash, '0', function() runLayout(layouts.coding) end)
-hs.hotkey.bind(mash, '9', function() runLayout(layouts.alternatecoding) end)
+if isWorkMachine() then
+  hs.hotkey.bind(mash, '0', function() runLayout(layouts.work) end)
+  hs.hotkey.bind(mash, '9', function() runLayout(layouts.alternatework) end)
+else
+  hs.hotkey.bind(mash, '0', function() runLayout(layouts.coding) end)
+  hs.hotkey.bind(mash, '9', function() runLayout(layouts.alternatecoding) end)
+end
 hs.hotkey.bind(mash, '8', function() runLayout(layouts.writing) end)
 
 -------------------------------------------------------------------
@@ -109,9 +147,12 @@ end)
 launchMode:bind({ 'ctrl' }, 'space', function() leaveMode() end)
 
 -- Mapped keys
+launchMode:bind({}, 'c',  function() switchToApp('Google Chrome.app') end)
 launchMode:bind({}, 'f',  function() switchToApp('Firefox.app') end)
 launchMode:bind({}, 'k',  function() switchToApp('Skim.app') end)
 launchMode:bind({}, 'l',  function() switchToApp('VLC.app') end)
+launchMode:bind({}, 'o',  function() switchToApp('Microsoft Outlook.app') end)
+launchMode:bind({}, 's',  function() switchToApp('Slack.app') end)
 launchMode:bind({}, 't',  function() switchToApp('iTerm.app') end)
 launchMode:bind({}, 'v',  function() switchToApp('VimR.app') end)
 launchMode:bind({}, 'w',  function() switchToApp('WhatsApp.app') end)
@@ -119,7 +160,7 @@ launchMode:bind({}, 'w',  function() switchToApp('WhatsApp.app') end)
 -- Unmapped keys
 launchMode:bind({}, 'a',  function() leaveMode() end)
 launchMode:bind({}, 'b',  function() leaveMode() end)
-launchMode:bind({}, 'c',  function() leaveMode() end)
+
 launchMode:bind({}, 'd',  function() leaveMode() end)
 launchMode:bind({}, 'e',  function() leaveMode() end)
 
@@ -131,11 +172,11 @@ launchMode:bind({}, 'j',  function() leaveMode() end)
 
 launchMode:bind({}, 'm',  function() leaveMode() end)
 launchMode:bind({}, 'n',  function() leaveMode() end)
-launchMode:bind({}, 'o',  function() leaveMode() end)
+
 launchMode:bind({}, 'p',  function() leaveMode() end)
 launchMode:bind({}, 'q',  function() leaveMode() end)
 launchMode:bind({}, 'r',  function() leaveMode() end)
-launchMode:bind({}, 's',  function() leaveMode() end)
+
 
 launchMode:bind({}, 'u',  function() leaveMode() end)
 
